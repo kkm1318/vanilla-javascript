5f8fad485075556a728a85efe71887db873f66c4
kyungminKim <kyungmin.k@qcells.com>
2024-10-08 16:54:08 +0900

    add: todo list management

---
+++ js/todo.js
@@ -0,0 +1,57 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.getElementById("todo-list");
+
+const TODOS_KEY = "todos";
+
+let toDos = [];
+
+const saveToDos = () => {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+};
+
+const deleteToDo = (event) => {
+  const li = event.target.parentElement;
+  li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveToDos();
+};
+
+const paintToDo = (newTodoObj) => {
+  const li = document.createElement("li");
+  li.id = newTodoObj.id;
+  const span = document.createElement("span");
+  span.innerText = newTodoObj.text;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteToDo);
+  li.appendChild(span);
+  li.appendChild(button);
+  toDoList.appendChild(li);
+};
+
+const handleToDoSubmit = (event) => {
+  event.preventDefault();
+  const newTodo = toDoInput.value;
+  toDoInput.value = "";
+  const newTodoObj = {
+    id: Date.now(),
+    text: newTodo,
+  };
+  toDos.push(newTodoObj);
+  paintToDo(newTodoObj);
+  saveToDos();
+};
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const initToDos = () => {
+  const loadedToDos = localStorage.getItem(TODOS_KEY);
+  if (loadedToDos !== null) {
+    const parsedToDos = JSON.parse(loadedToDos);
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo);
+  }
+};
+
+initToDos();
